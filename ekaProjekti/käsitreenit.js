8aab8c8be27899d1cb07a26d756dd3f9922f44cc
TeroSimonen <terosimon@gmail.com>
2020-12-10 20:10:42 +0200

    Päivitetty käsitreenit

---
+++ ekaProjekti/käsitreenit.js
@@ -0,0 +1,25 @@
+  
+// Otetaan mongoose käyttöön
+const mongoose = require("mongoose");
+// Luodaan skeema
+const Schema = mongoose.Schema;
+// Luodaan käsitreenit-skeema
+let käsitreenit = new Schema ( {
+
+    liikkeet: [{
+        liike: {type: String}
+       },
+       {
+        sarjat: {type: Number}
+       },
+       {
+        toistot: {type: Number}
+       }
+       ]
+},
+
+        { collection: "käsitreenit"}
+);
+
+// Tarjoa pääsy tähän 
+module.exports = mongoose.model("käsitreenit", käsitreenit);
